b648abd5831562e579334e9ac4734b40c1992c49
Robert MacLean <robert@sadev.co.za>
2015-09-29 19:05:29 +0200

    Initial commit

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace App1
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public Customer[] Names => new Customer[] { "Robert", "Billy", "Leslie", "Doug" };
+
+        public async void Click()
+        {
+            await new MessageDialog("!").ShowAsync();
+        }
+
+
+    }
+
+    public class Customer
+    {
+        public string Title { get; set; }
+
+        public async void Clicky()
+        {
+            await new MessageDialog("!").ShowAsync();
+        }
+
+        public static implicit operator Customer(string s) => new Customer
+        {
+            Title = s
+        };
+    }
+}
